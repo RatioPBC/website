27bc48328779833d09ed491f142adce48787686b
Jesse Cooke <jesse@ratiopbc.com>
2025-07-15 18:14:26 -0700

    Clean out old site

--- src/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-import ratioIcon from '@/images/logos/ratio.svg'
-
-
-export function Logo(props) {
-  return (
-    <Image src={ratioIcon} alt="Ratio PBC logo" {...props}/>
-  )
-}
